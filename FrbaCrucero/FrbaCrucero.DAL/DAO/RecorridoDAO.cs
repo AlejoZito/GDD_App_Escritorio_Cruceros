e24589554d85b7645511bb1891ffd0223a30b35d
Agustín Coda <agustino996@gmail.com>
2019-06-07 17:38:19 -0300

    RecorridoDAO Insertar nuevo recorrido

--- FrbaCrucero/FrbaCrucero.DAL/DAO/RecorridoDAO.cs
+++ FrbaCrucero/FrbaCrucero.DAL/DAO/RecorridoDAO.cs
@@ -40,12 +40,25 @@ namespace FrbaCrucero.DAL.DAO
                 comando.Parameters.Add("@activo", SqlDbType.Bit);
                 comando.Parameters["@activo"].Value = recorrido.Activo;
                 int idRecorrido = Convert.ToInt32(comando.ExecuteScalar());
+                int orden = 1;
 
                 //Inserto los tramos en base con el id de recorrido
                 foreach (Tramo tramo in recorrido.Tramos)
                 {
                     comando = new SqlCommand(@"INSERT INTO TIRANDO_QUERIES.Tramo(tram_recorrido, tram_puerto_desde, tram_puerto_hasta, tram_precio, tram_orden) VALUES(@cod_recorrido, @cod_puerto_desde, @cod_puerto_hasta, @precio, @orden)", conn);
+                    comando.Parameters.Add("@tram_recorrido", SqlDbType.Int);
+                    comando.Parameters["@tram_recorrido"].Value = idRecorrido;
+                    comando.Parameters.Add("@tram_puerto_desde", SqlDbType.Int);
+                    comando.Parameters["@tram_puerto_desde"].Value = tramo.Puerto_Desde.Cod_Puerto;
+                    comando.Parameters.Add("@tram_puerto_hasta", SqlDbType.Int);
+                    comando.Parameters["@tram_puerto_hasta"].Value = tramo.Puerto_Hasta.Cod_Puerto;
+                    comando.Parameters.Add("@tram_precio", SqlDbType.Decimal);
+                    comando.Parameters["@tram_precio"].Value = tramo.Precio;
+                    comando.Parameters.Add("@tram_orden", SqlDbType.Int);
+                    comando.Parameters["@tram_orden"].Value = orden;
+                    orden++;
 
+                    comando.ExecuteNonQuery();
                 }
 
                 comando.Dispose();
