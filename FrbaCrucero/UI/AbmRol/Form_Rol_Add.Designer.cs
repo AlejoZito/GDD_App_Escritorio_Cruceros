cbd7031a9adff454f6961a32a90a681bafa4b2fb
Agustín Coda <agustino996@gmail.com>
2019-06-20 13:36:17 -0300

    Agrego parte visual al Add de Rol

--- FrbaCrucero/UI/AbmRol/Form_Rol_Add.Designer.cs
+++ FrbaCrucero/UI/AbmRol/Form_Rol_Add.Designer.cs
@@ -28,11 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listViewPermisos = new FrbaCrucero.UI._Components.DataListViewControl();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(103, 23);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(171, 20);
+            this.tbNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Permisos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 66);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Seleccione más de 1 permiso utilizando ctrl o shift";
+            // 
+            // listViewPermisos
+            // 
+            this.listViewPermisos.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listViewPermisos.HideSelection = false;
+            this.listViewPermisos.Location = new System.Drawing.Point(29, 102);
+            this.listViewPermisos.Margin = new System.Windows.Forms.Padding(2);
+            this.listViewPermisos.Name = "listViewPermisos";
+            this.listViewPermisos.Size = new System.Drawing.Size(311, 144);
+            this.listViewPermisos.TabIndex = 6;
+            this.listViewPermisos.UseCompatibleStateImageBehavior = false;
+            this.listViewPermisos.View = System.Windows.Forms.View.Details;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(248, 278);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(92, 29);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // Form_Rol_Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form_Rol_Add";
+            this.ClientSize = new System.Drawing.Size(353, 344);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.listViewPermisos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_Rol_Add";
+            this.Text = "Agregar Rol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private _Components.DataListViewControl listViewPermisos;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
